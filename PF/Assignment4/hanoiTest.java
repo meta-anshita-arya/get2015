9d785b3fdf642238fd7c40b225e76ed046a77e2a
meta-anshita-arya <anshita.arya@metacube.com>
2015-08-14 15:48:12 +0530

    Update hanoiTest.java

--- PF/Assignment4/hanoiTest.java
+++ PF/Assignment4/hanoiTest.java
@@ -6,10 +6,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class hanoiTest {
-	
-	hanoi obj=new hanoi();
+
+	hanoi obj = new hanoi();
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -28,29 +27,29 @@ public class hanoiTest {
 	}
 
 	@Test
-	public void test() {                                              //test for 1 disk
-		
-		        String a=obj.towerofhanoi(1,"A","B","C");
-				String b="move 1 from A to C,";
-				assertEquals(b,a);
+	public void test() { // test for 1 disk
+
+		String a = obj.towerofhanoi(1, "A", "B", "C");
+		String b = "move 1 from A to C,";
+		assertEquals(b, a);
 	}
 
 	@Test
-	public void test1() {                                                //test for 2 disk
-		
-		    String a=obj.towerofhanoi(2,"A","B","C");
-			String b="move 1 from A to B,move 2 from A to C,move 1 from B to C,";
-			assertEquals(b,a);
-		
+	public void test1() { // test for 2 disk
+
+		String a = obj.towerofhanoi(2, "A", "B", "C");
+		String b = "move 1 from A to B,move 2 from A to C,move 1 from B to C,";
+		assertEquals(b, a);
+
 	}
 
 	@Test
-	public void test2() {                                                    //test for 3 disk
-		
-		    String a=obj.towerofhanoi(3,"A","B","C");
-			String b="move 1 from A to C,move 2 from A to B,move 1 from C to B,move 3 from A to C,move 1 from B to A,move 2 from B to C,move 1 from A to C,";
-			assertEquals(b,a);
-		
+	public void test2() { // test for 3 disk
+
+		String a = obj.towerofhanoi(3, "A", "B", "C");
+		String b = "move 1 from A to C,move 2 from A to B,move 1 from C to B,move 3 from A to C,move 1 from B to A,move 2 from B to C,move 1 from A to C,";
+		assertEquals(b, a);
+
 	}
 
 }
