8c6590913da6724cde90e5df3c6d6d3314105ea7
meta-anshita-arya <anshita.arya@metacube.com>
2015-08-14 15:49:34 +0530

    Update permutationTest.java

--- PF/Assignment4/permutationTest.java
+++ PF/Assignment4/permutationTest.java
@@ -6,10 +6,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class permutationTest {
-	
-	permutation obj=new permutation();
+
+	permutation obj = new permutation();
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -28,20 +27,20 @@ public class permutationTest {
 	}
 
 	@Test
-	public void test() {                                                 //test for "ABC"
-		
-		        String a=obj.generatePermutations("","ABC");
-				String b=" ABC ACB BAC BCA CAB CBA";
-				assertEquals(b,a);
+	public void test() { // test for "ABC"
+
+		String a = obj.generatePermutations("", "ABC");
+		String b = " ABC ACB BAC BCA CAB CBA";
+		assertEquals(b, a);
 	}
 
 	@Test
-	public void test1() {                                                    //test for "ABCD"
-		
-		    String a=obj.generatePermutations("","ABCD");
-			String b=" ABCD ABDC ACBD ACDB ADBC ADCB BACD BADC BCAD BCDA BDAC BDCA CABD CADB CBAD CBDA CDAB CDBA DABC DACB DBAC DBCA DCAB DCBA";
-			assertEquals(b,a);
-		
+	public void test1() { // test for "ABCD"
+
+		String a = obj.generatePermutations("", "ABCD");
+		String b = " ABCD ABDC ACBD ACDB ADBC ADCB BACD BADC BCAD BCDA BDAC BDCA CABD CADB CBAD CBDA CDAB CDBA DABC DACB DBAC DBCA DCAB DCBA";
+		assertEquals(b, a);
+
 	}
 
 }
