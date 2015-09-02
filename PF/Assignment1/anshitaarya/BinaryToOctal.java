e1ffa8fb003cc59394f3949e5f5ff338602e51be
meta-anshita-arya <anshita.arya@metacube.com>
2015-09-02 10:56:22 +0530

    Update BinaryToOctal.java

--- PF/Assignment1/anshitaarya/BinaryToOctal.java
+++ PF/Assignment1/anshitaarya/BinaryToOctal.java
@@ -1,16 +1,11 @@
 package anshitaarya;
 
 import java.util.*;
-
-
-
 public class BinaryToOctal {
-
 	private static Scanner in;
-
 	public static void main (String args[])
 	{
-		in = new Scanner(System.in);
+        	in = new Scanner(System.in);
 		 int num,decimal,octal;
 		 assignment1 obj=new assignment1();
 		 System.out.println("enter binary number");
@@ -19,24 +14,17 @@ public class BinaryToOctal {
 		 octal= obj. convertTooctale(decimal);
 		 System.out.println("dec number is"+decimal);
 		 System.out.println("Octal number is"+octal);
-	
 	}
 	
 	int convertTodecimal (int x)
-	{
-		
-	int decimal=0,i=1,rem;
+	{	
+	        int decimal=0,i=1,rem;
 		while(x!=0)
 		{
-		rem= x%10;
-		
-		
-		decimal=((rem*i)+decimal );
-		
-		x=x/10;
-		
-		i=i*2;
-		
+		        rem= x%10;
+	         	decimal=((rem*i)+decimal );
+	           	x=x/10;
+	         	i=i*2;
 		}
 		return (decimal);
 	}
@@ -46,12 +34,10 @@ public class BinaryToOctal {
 		int octal=0, rem,i=1 ;
 		while(y!=0)
 		{
-		rem= y%8;
-		
-
-		octal=octal + (rem*i);
-		y= y/8;
-		i=i*10;
+	        	rem= y%8;
+		        octal=octal + (rem*i);
+		        y= y/8;
+	         	i=i*10;
 		}
 		return (octal);
 }
