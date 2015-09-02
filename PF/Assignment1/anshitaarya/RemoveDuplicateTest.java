aedb54edef8c6f2e1f482df9874fa44a4467b874
meta-anshita-arya <anshita.arya@metacube.com>
2015-09-02 10:47:39 +0530

    Update and rename prg2Test.java to RemoveDuplicateTest.java

--- PF/Assignment1/anshitaarya/RemoveDuplicateTest.java
+++ PF/Assignment1/anshitaarya/RemoveDuplicateTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class prg2Test {
+public class RemoveDuplicateTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
