ed11e70394063ef3f746aae78467b8fad0aebdf0
meta-anshita-arya <anshita.arya@metacube.com>
2015-09-02 10:49:20 +0530

    Update and rename prg3Test.java to FindLongestSequenceTest.java

--- PF/Assignment1/anshitaarya/FindLongestSequenceTest.java
+++ PF/Assignment1/anshitaarya/FindLongestSequenceTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class prg3Test {
+public class FindLongestSequenceTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
