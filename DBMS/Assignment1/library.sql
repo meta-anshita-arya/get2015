98f328550110ba958b5113c150838b12d26385d5
meta-anshita-arya <anshita.arya@metacube.com>
2015-09-08 18:35:24 +0530

    Update library.sql

--- DBMS/Assignment1/library.sql
+++ DBMS/Assignment1/library.sql
@@ -1,5 +1,8 @@
+/* created database*/
 CREATE DATABASE Library_Management;
 USE Library_Management;
+
+/* created table named members*/
 CREATE TABLE members(
 member_id INT PRIMARY KEY AUTO_INCREMENT,
 member_nm VARCHAR(20),
@@ -7,18 +10,22 @@ addressline1 VARCHAR(20),
 addressline2 VARCHAR(20),
 category CHAR);
 
+/* created table named authors*/
 CREATE TABLE authors(
 author_id INT PRIMARY KEY AUTO_INCREMENT,
 author_nm VARCHAR(20));
 
+/* created table named publishers*/
 CREATE TABLE publishers(
 publisher_id INT PRIMARY KEY AUTO_INCREMENT,
 publisher_nm VARCHAR(20));
 
+/* created table named subjects*/
 CREATE TABLE subjects(
 subject_id INT PRIMARY KEY AUTO_INCREMENT,
 subject_nm VARCHAR(20));
 
+/* created table named titles*/
 CREATE TABLE titles(
 title_id INT PRIMARY KEY AUTO_INCREMENT,
 title_nm VARCHAR(20),
@@ -30,6 +37,7 @@ FOREIGN KEY(publisher_id)REFERENCES publishers(publisher_id)
 ON DELETE CASCADE
 );
 
+/* created table named title_author*/
 CREATE TABLE title_author(
 title_id INT,
 author_id INT,
@@ -37,6 +45,7 @@ PRIMARY KEY(title_id,author_id),
 FOREIGN KEY(title_id) REFERENCES titles(title_id),
 FOREIGN KEY(author_id) REFERENCES authors(author_id));
 
+/* created table named book*/
 CREATE TABLE books(
 accession_no INT PRIMARY KEY AUTO_INCREMENT,
 title_id INT,
@@ -46,6 +55,7 @@ status INT,
 FOREIGN KEY(title_id) REFERENCES titles(title_id) ON DELETE CASCADE
 );
 
+/* created table named book_issue*/
 CREATE TABLE book_issue(
 issue_dt DATE,
 accession_no INT,
@@ -58,7 +68,7 @@ FOREIGN KEY(member_id) REFERENCES members(member_id)
 ON DELETE CASCADE
 );
 
-
+/* created table named book_return*/
 CREATE TABLE book_return(
 return_dt DATE,
 issue_dt DATE,
@@ -71,10 +81,11 @@ FOREIGN KEY(member_id) REFERENCES members(member_id)
 ON DELETE CASCADE
 );
 
-
+/* altering table named book_issue*/
 ALTER TABLE book_issue
 MODIFY COLUMN issue_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP();
 
+/*created trigger with name book*/
 CREATE TRIGGER book
 BEFORE
 INSERT 
@@ -82,18 +93,21 @@ ON book_issue
 FOR EACH ROW 
 SET NEW.due_dt = NOW() + INTERVAL 15 day;
 
-
+/*display table*/
 SHOW CREATE TABLE book_issue;
 
+/* altering table named book_issue*/
 ALTER TABLE book_issue
 DROP FOREIGN KEY member_id_first;
 
+/* altering table named book_return*/
 ALTER TABLE book_return
 DROP FOREIGN KEY member_id_second;
 
-
+/*drop member table*/
 DROP TABLE members;
 
+/*create member table*/
 CREATE TABLE members(
 member_id INT PRIMARY KEY AUTO_INCREMENT,
 member_nm VARCHAR(20),
@@ -101,13 +115,14 @@ addressline1 VARCHAR(20),
 addressline2 VARCHAR(20),
 category CHAR);
 
+/* altering table named book_issue*/
 ALTER TABLE book_issue
 ADD CONSTRAINT member_id_first
 FOREIGN KEY (member_id)
 REFERENCES members(member_id);
 
+/* altering table named book_return*/
 ALTER TABLE book_return
 ADD CONSTRAINT member_id_second
 FOREIGN KEY (member_id)
 REFERENCES members(member_id);
-
