d39d568c0edaced609a2a3cd919a03ac60b53bc9
meta-anshita-arya <anshita.arya@metacube.com>
2015-09-23 14:44:27 +0530

    Update CarJdbcHelper.java

--- PF/Assignment1/CarJdbcHelper.java
+++ PF/Assignment1/CarJdbcHelper.java
@@ -8,63 +8,65 @@ public class CarJdbcHelper {
 
 	public static void insertInCarTable(Car objCar)
 			throws VehicleManagementException {
-		Connection con = null;
-		Statement stmt = null;
-		PreparedStatement ps = null;
-		ConnectionUtil conUtil = new ConnectionUtil();
+		Connection connection = null;
+		Statement statement = null;
+		PreparedStatement preparedStatement = null;
+		ConnectionUtil connectionUtil = new ConnectionUtil();
 		/* creates connection to db */
-		con = conUtil.getConnection();
-		ResultSet rs = null;
+		connection = connectionUtil.getConnection();
+		ResultSet resultSet = null;
 
 		String query = "INSERT INTO Vehicle (make, model, engine_in_cc, fuel_capacity, milage, price, road_tax) VALUES (?,?,?,?,?,?,?)";
 
 		try {
-			ps = (PreparedStatement) con.prepareStatement(query);
+			preparedStatement = (PreparedStatement) connection
+					.prepareStatement(query);
 			/* set variable in prepared statement */
-			ps.setString(1, objCar.getMake());
-			ps.setString(2, objCar.getModel());
-			ps.setDouble(3, objCar.getEngineInCC());
-			ps.setDouble(4, objCar.getFuelCapacity());
-			ps.setDouble(5, objCar.getMilage());
-			ps.setDouble(6, objCar.getPrice());
-			ps.setDouble(7, objCar.getRoadTax());
-			ps.executeUpdate();
+			preparedStatement.setString(1, objCar.getMake());
+			preparedStatement.setString(2, objCar.getModel());
+			preparedStatement.setDouble(3, objCar.getEngineInCC());
+			preparedStatement.setDouble(4, objCar.getFuelCapacity());
+			preparedStatement.setDouble(5, objCar.getMilage());
+			preparedStatement.setDouble(6, objCar.getPrice());
+			preparedStatement.setDouble(7, objCar.getRoadTax());
+			preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
-		ps = null;
+		preparedStatement = null;
 
 		String query1 = "SELECT make FROM Vehicle";
 		String query2 = "INSERT INTO Car ( ac, power_steering, accessory_kit, vehicle_id ) VALUES (?,?,?,?)";
 
 		try {
-			stmt = con.createStatement();
-			rs = stmt.executeQuery(query1);
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(query1);
 			int vehicle_id = 0;
-			while (rs.next())
+			while (resultSet.next())
 				vehicle_id += 1;
 			System.out.println(vehicle_id);
-			ps = (PreparedStatement) con.prepareStatement(query2);
+			preparedStatement = (PreparedStatement) connection
+					.prepareStatement(query2);
 			/* set variable in prepared statement */
-			ps.setBoolean(1, objCar.getAC());
-			ps.setBoolean(2, objCar.getPowerSteering());
-			ps.setBoolean(3, objCar.getAccessoryKit());
-			ps.setInt(4, vehicle_id);
-			ps.executeUpdate();
+			preparedStatement.setBoolean(1, objCar.getAC());
+			preparedStatement.setBoolean(2, objCar.getPowerSteering());
+			preparedStatement.setBoolean(3, objCar.getAccessoryKit());
+			preparedStatement.setInt(4, vehicle_id);
+			preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			/* close connection */
 			try {
-				if (con != null) {
-					con.close();
+				if (connection != null) {
+					connection.close();
 				}
-				if (ps != null) {
-					ps.close();
+				if (preparedStatement != null) {
+					preparedStatement.close();
 				}
-				if (stmt != null) {
-					stmt.close();
+				if (statement != null) {
+					statement.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
