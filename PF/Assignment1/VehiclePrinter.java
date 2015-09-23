2f5f76fd8a4f19813e1c26bd83e8d19b62f58244
meta-anshita-arya <anshita.arya@metacube.com>
2015-09-23 11:34:36 +0530

    Create VehiclePrinter.java

---
+++ PF/Assignment1/VehiclePrinter.java
@@ -0,0 +1,10 @@
+// prints objects of various vehicle classes
+
+public class VehiclePrinter {
+
+	public static String printVehicleSpecification(Vehicle vehicleObj) {
+		System.out.println(vehicleObj);
+		return vehicleObj.toString();
+	}
+
+}
