cfd74dba22e9b006cb31f25b0ff0bd4b49cbfb98
meta-anshita-arya <anshita.arya@metacube.com>
2015-09-23 11:28:49 +0530

    Create VehicleHelperTest.java

---
+++ PF/Assignment1/VehicleHelperTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VehicleHelperTest {
+
+	@Test
+	public void testCreationOfCarInstance() {
+		Vehicle vehicleObj = VehicleHelper.createVehicle("Car", "Make2",
+				"Model2");
+		// assertNotNull(vehicleObj);
+		assertTrue(vehicleObj instanceof Car);
+	}
+
+	@Test
+	public void testCreationOfBikeInstance() {
+		Vehicle vehicleObj = VehicleHelper.createVehicle("Bike", "Make3",
+				"Model3");
+		// assertNotNull(vehicleObj);
+		assertTrue(vehicleObj instanceof Bike);
+	}
+
+}
