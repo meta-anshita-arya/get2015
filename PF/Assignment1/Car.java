cee6516eb8341e5e8bc0d12377ecac3c3656be48
meta-anshita-arya <anshita.arya@metacube.com>
2015-09-23 16:51:44 +0530

    Update Car.java

--- PF/Assignment1/Car.java
+++ PF/Assignment1/Car.java
@@ -1,12 +1,11 @@
-// car class
-
 public class Car extends Vehicle {
-
 	private boolean AC;
 	private boolean powerSteering;
 	private boolean accessoryKit;
 
-	public boolean getAC() {
+	// getters and setters
+
+	public boolean isAC() {
 		return AC;
 	}
 
@@ -14,7 +13,7 @@ public class Car extends Vehicle {
 		AC = aC;
 	}
 
-	public boolean getPowerSteering() {
+	public boolean isPowerSteering() {
 		return powerSteering;
 	}
 
@@ -22,55 +21,12 @@ public class Car extends Vehicle {
 		this.powerSteering = powerSteering;
 	}
 
-	public boolean getAccessoryKit() {
+	public boolean isAccessoryKit() {
 		return accessoryKit;
 	}
 
 	public void setAccessoryKit(boolean accessoryKit) {
 		this.accessoryKit = accessoryKit;
-	}
-
-	// override calculateOnRoadPrice of vehicle
-	double calculateOnRoadPrice() {
-		return super.calculateOnRoadPrice() + 200;
-	}
-
-	@Override
-	public String toString() {
-		return "Car [Make=" + getMake() + ", Model=" + getModel()
-				+ ", Engine In CC=" + getEngineInCC() + ", Fuel Capacity"
-				+ getFuelCapacity() + ", Milage=" + getMilage() + ", Price="
-				+ getPrice() + ", Road Tax=" + getRoadTax() + ", AC=" + AC
-				+ ", Power Steering=" + powerSteering + ", Accessory Kit="
-				+ accessoryKit + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + (AC ? 1231 : 1237);
-		result = prime * result + (accessoryKit ? 1231 : 1237);
-		result = prime * result + (powerSteering ? 1231 : 1237);
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (!(obj instanceof Car))
-			return false;
-		Car other = (Car) obj;
-		if (AC != other.AC)
-			return false;
-		if (accessoryKit != other.accessoryKit)
-			return false;
-		if (powerSteering != other.powerSteering)
-			return false;
-		return true;
 	}
 
 }
