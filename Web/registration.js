6cbf2ea7f9fa87c9ff02aa662c4820034a01a679
meta-anshita-arya <anshita.arya@metacube.com>
2015-10-07 16:01:18 +0530

    Update registration.js

--- Web/registration.js
+++ Web/registration.js
@@ -40,24 +40,24 @@ function stateComboChange()
                         break;
             case '0':   addOption(combo2, 0, 'Jaipur');
                         addOption(combo2, 1, 'Ajmer');
-						addOption(combo2, 2, 'Kota');
-						addOption(combo2, 3, 'Jodhpur');
+			addOption(combo2, 2, 'Kota');
+			addOption(combo2, 3, 'Jodhpur');
                         break;
             case '1':   addOption(combo2, 4, 'Bhopal');
                         addOption(combo2, 5, 'Indore');
-						addOption(combo2, 6, 'Mandsor');
-						addOption(combo2, 7, 'Gwalior');
+			addOption(combo2, 6, 'Mandsor');
+			addOption(combo2, 7, 'Gwalior');
                         break;
             case '2':   addOption(combo2, 8, 'Kanpur');
                         addOption(combo2, 9, 'Lucknow');
-						addOption(combo2, 10, 'Noida');
-						addOption(combo2, 11, 'Merut');
+			addOption(combo2, 10, 'Noida');
+			addOption(combo2, 11, 'Merut');
                         break;
-			case '3': addOption(combo2, 9, 'Nasik');
-					  addOption(combo2, 9, 'Mumbai');
-					  addOption(combo2, 9, 'Pune');
-					  addOption(combo2, 9, 'Nagpur');
-					  break;
+	    case '3':   addOption(combo2, 12, 'Nasik');
+			addOption(combo2, 13, 'Mumbai');
+	       		addOption(combo2, 14, 'Pune');
+			addOption(combo2, 15, 'Nagpur');
+			break;
         }
         cityComboChange();
     }
