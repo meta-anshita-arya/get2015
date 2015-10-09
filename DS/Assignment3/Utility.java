7c91d0b3416cae00d9622dbcda97cd6c314d6a5e
meta-anshita-arya <anshita.arya@metacube.com>
2015-10-09 14:19:23 +0530

    Create Utility.java

---
+++ DS/Assignment3/Utility.java
@@ -0,0 +1,77 @@
+package puzzle;
+
+import java.util.Scanner;
+public class Utility 
+{
+	public static int getIntegerValue(String message)
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		do
+		{
+			System.out.println(message);
+			int number;
+			String inputStr = sc.next();
+			if(isNumeric(inputStr) && (Integer.parseInt(inputStr)>0))
+			{
+				number=Integer.parseInt(inputStr);
+				return number;
+			}
+			else
+			{
+				System.out.println("Enter positive integer value");
+			}
+		}while(true);
+	}
+	
+	public static boolean isNumeric(String input) 
+	{
+		try 
+		{
+			Integer.parseInt(input);
+		} 
+		catch (NumberFormatException e) 
+		{
+			return false;
+		}
+		
+		return true;	
+	}
+	
+	public static String getStringInput(String message)
+	{
+		System.out.println(message);
+		Scanner sc=new Scanner(System.in);
+		String inputStr = sc.nextLine();
+		return inputStr;
+	}
+	
+	public static char getCharacterInput(String message)
+	{
+		System.out.println(message);
+		Scanner sc=new Scanner(System.in);
+		char input = sc.next().charAt(0);
+		if((input == 'y' || input == 'Y') || (input == 'n' || input == 'N'))
+		{
+			return input;
+		}
+		else
+		{	
+			return getCharacterInput(message);
+		
+		}
+		
+	}
+	public static int[] getArrayInput(String message)
+	{
+		System.out.println(message);
+		Scanner sc=new Scanner(System.in);
+		int array[] = new int[9];
+		for(int i = 0; i < array.length ; i++)
+		{
+			array[i] = sc.nextInt();
+		}
+		return array;
+	}
+	
+}
